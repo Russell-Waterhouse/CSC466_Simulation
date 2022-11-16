57905dcff69a972c5fcf33cf6fcd357217edeb3b
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2022-11-15 23:23:42 -0800

    initial commit of mininet config

---
+++ main.py
@@ -0,0 +1,27 @@
+from mininet.topo import Topo
+
+
+class MyTopo(Topo):
+    """Topology of network test"""
+
+    def build(self):
+        """Create the custom network topology"""
+
+        org_switches = []
+        for i in range(0, 5):
+            h1 = self.addHost('h1_' + str(i))
+            h2 = self.addHost('h2_' + str(i))
+            h3 = self.addHost('h3_' + str(i))
+            h4 = self.addHost('h4_' + str(i))
+            org_switch = self.addSwitch('s' + str(i))
+            self.addLink(h1, org_switch)
+            self.addLink(h2, org_switch)
+            self.addLink(h3, org_switch)
+            self.addLink(h4, org_switch)
+            org_switches.append(org_switch)
+        isp_switch = self.addSwitch('s999')
+        for switch in org_switches:
+            self.addLink(isp_switch, switch)
+
+
+topos = {'mytopo': (lambda: MyTopo())}
