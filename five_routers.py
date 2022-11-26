c2eb9feba199d341eb21bdc5fd88fac389abb5b5
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2022-11-25 16:56:16 -0800

    remove broken route from r3 to r2 through r1

--- five_routers.py
+++ five_routers.py
@@ -73,9 +73,8 @@ def run():
     # Add routing for reaching networks that aren't directly connected
     info(net['r1'].cmd("ip route add 10.1.0.0/24 via 10.100.0.2 dev r1-eth2")) #r1 -> r2
     info(net['r2'].cmd("ip route add 10.0.0.0/24 via 10.100.0.1 dev r2-eth2")) #r2 -> r1
-    info(net['r3'].cmd("ip route add 10.0.0.0/24 via 10.101.0.1 dev r3-eth2")) #r3 -> r1
     info(net['r1'].cmd("ip route add 10.3.0.0/24 via 10.101.0.2 dev r1-eth3")) #r1 -> r3
-    info(net['r3'].cmd("ip route add 10.1.0.0/24 via 10.101.0.1 dev r3-eth2")) #r3 -> r1 -> r2
+    info(net['r3'].cmd("ip route add 10.0.0.0/24 via 10.101.0.1 dev r3-eth2")) #r3 -> r1
 
 
     net.start()
