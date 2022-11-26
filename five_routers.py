36d2b3c4e60a09635c0c09624631e7b1fdfa0d2b
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2022-11-25 16:34:16 -0800

    Add third subnet

--- five_routers.py
+++ five_routers.py
@@ -11,10 +11,12 @@ class TwoRouters(Topo):
         # Add 2 routers in two different subnets
         r1 = self.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/24')
         r2 = self.addHost('r2', cls=LinuxRouter, ip='10.1.0.1/24')
+        r3 = self.addHost('r3', cls=LinuxRouter, ip='10.3.0.1/24')
 
         # Add 2 switches
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
 
         # Add host-switch links in the same subnet
         self.addLink(s1,
@@ -27,6 +29,11 @@ class TwoRouters(Topo):
                      intfName2='r2-eth1',
                      params2={'ip': '10.1.0.1/24'})
 
+        self.addLink(s3,
+                     r3,
+                     intfName2='r3-eth1',
+                     params2={'ip': '10.3.0.1/24'})
+
         # Add router-router link in a new subnet for the router-router connection
         self.addLink(r1,
                      r2,
@@ -35,6 +42,13 @@ class TwoRouters(Topo):
                      params1={'ip': '10.100.0.1/24'},
                      params2={'ip': '10.100.0.2/24'})
 
+        # self.addLink(r1,
+        #              r3,
+        #              intfName1='r1-eth2',
+        #              intfName2='r3-eth2',
+        #              params1={'ip': '10.100.0.1/24'},
+        #              params2={'ip': '10.100.0.3/24'})
+
         # Adding hosts specifying the default route
         d1 = self.addHost(name='d1',
                           ip='10.0.0.251/24',
@@ -42,10 +56,15 @@ class TwoRouters(Topo):
         d2 = self.addHost(name='d2',
                           ip='10.1.0.252/24',
                           defaultRoute='via 10.1.0.1')
+        d3 = self.addHost(name='d3',
+                          ip='10.1.0.253/24',
+                          defaultRoute='via 10.3.0.1')
+
 
         # Add host-switch links
         self.addLink(d1, s1)
         self.addLink(d2, s2)
+        self.addLink(d3, s3)
 
 def run():
     topo = TwoRouters()
