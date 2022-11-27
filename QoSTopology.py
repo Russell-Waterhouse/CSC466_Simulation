5438d14f36771b436db9fa887d9f3910b1c9ba92
Samsara <alex116.dll@gmail.com>
2022-11-27 00:55:58 -0800

    fixed ip prefix

--- QoSTopology.py
+++ QoSTopology.py
@@ -3,7 +3,6 @@ from mininet.topo import Topo
 ORG_COUNT = 3
 HOST_COUNT = 5
 
-
 class QoSTopology(Topo):
     """Topology of network test"""
 
@@ -13,10 +12,11 @@ class QoSTopology(Topo):
         isp_switch = self.addSwitch('s999')
         for org_id in range(ORG_COUNT):
             org_switch = self.addSwitch(f's{org_id}')
-            self.addLink(isp_switch, org_switch)
+            self.addLink(org_switch, isp_switch)
             for host_id in range(HOST_COUNT):
-                host = self.addHost(f'h{org_id}_{host_id}')
-                self.addLink(host, org_switch)
+                host_ip = f"10.0.{org_id}.{host_id+1}/48"
+                host = self.addHost(f'h{org_id}_{host_id}', ip=host_ip)
+                self.addLink(org_switch, host)
 
 
 topos = {'QoSTopology': (lambda: QoSTopology())}
