6fb94332407c11f31764e98deca3629b75168a75
Samsara <alex116.dll@gmail.com>
2022-11-27 19:26:25 -0800

    cleanup

--- init_mininet_full.py
+++ init_mininet_full.py
@@ -4,18 +4,6 @@ from TrafficControl import *
 from mininet.net import Mininet
 from mininet.log import setLogLevel
 from mininet.cli import CLI
-from mininet.util import dumpNodeConnections
-
-
-def set_tc(isp_node, org_data):
-    # ISP tc
-    interfaces = []
-    for interface in interfaces:
-        setup_prioritization_interface(isp_node, interface)
-
-    org_data = []
-    for (node, interface) in org_data:
-        setup_delay_interface(node, interface)
 
 
 def main():
