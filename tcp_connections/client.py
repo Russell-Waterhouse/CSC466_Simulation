3945b92bb8164b1be5fe37359bee5d4a97d6c890
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2022-11-27 16:20:26 -0800

    Remove comment

--- tcp_connections/client.py
+++ tcp_connections/client.py
@@ -16,12 +16,3 @@ while True:
     c.send(bytes.fromhex("FF"))  # 0xFF in bytes
     print("sent bytes")
     time.sleep(5)
-
-
-
-# c = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-# print(c)
-# c.connect(("206.87.190.101", 9300))
-# name = input("Input your name")
-# c.send(bytes(name,'utf-8'))
-# print(c.recv(1024).decode())
