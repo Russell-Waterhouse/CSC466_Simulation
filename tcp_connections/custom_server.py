df2d14545d4d9a8de8a8dd067535d886f325d95f
Samsara <alex116.dll@gmail.com>
2022-11-30 20:51:44 -0800

    finished this stupid ass script

--- tcp_connections/custom_server.py
+++ tcp_connections/custom_server.py
@@ -1,11 +1,35 @@
 import socket
 import sys
+import time
+import threading
 
 # setting path
 sys.path.append('../CSC466_Simulation')
 
 import util
+
 settings = util.get_settings()["NetworkSimulation"]
+
+
+def listen(host, port, id):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        print("Socket Created")
+        print(s)
+        s.bind((host, port))
+        s.listen()
+        print('waiting for connections')
+        time0 = -1
+        while True:
+            c, addr = s.accept()
+            c.send(bytes.fromhex("00") * settings["PacketByteSize"])  # 0xFF in ascii
+            if (time0 == -1):
+                time0 = time.time()
+            print("Receive packet at time:", time.time() - time0, "at id", id)
+            c.close()
+    finally:
+        s.close()
+
 
 def main():
     port = util.get_settings()["NetworkSimulation"]["ConnectionPort"]
@@ -17,20 +41,10 @@ def main():
 
     host = sys.argv[1]
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        print("Socket Created")
-        print(s)
-        s.bind((host, port))
-        s.listen()
-        print('waiting for connections')
-        while True:
-            c, addr = s.accept()
-            print('connected with', addr)
-            c.send(bytes.fromhex("00") * settings["PacketByteSize"])  # 0xFF in ascii
-            c.close()
-    finally:
-        s.close()
+    connection1 = threading.Thread(target=listen, args=(host, 8000, "FAST"))
+    connection2 = threading.Thread(target=listen, args=(host, 7000, "SLOW"))
+    connection1.start()
+    connection2.start()
 
 
 if __name__ == '__main__':
